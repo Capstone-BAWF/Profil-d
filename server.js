f3fd47d526a716f9dfe3a793947baec7c32f4689
William Wu <williamwu0013@gmail.com>
2016-06-28 14:17:28 -0400

    Cleaned up server.js to prepare for new routes.

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
+// Load modules
 var express = require('express'), app = express();
 var mongoose = require('mongoose');
 var bodyParser= require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;
-// var path = require('path');
+var User = require('./app/models/user');
 
-// Define root directory
+// Body parser module will allow us to grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// Configure app to handle CORS requests
+app.use(function(req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'Get, POST');
+		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, \
+			Authorization');
+		next();
+});
+
+// Log requests to console
+app.use(morgan('dev'));
+
+// Routes for API
+
+// Root page
 app.get('/', function(req, res){
-		res.sendFile(path.join(__dirname + '/index.html'));
+		res.send('Welcome to the home page!');
 });
 
-// Creating admin routes
-var adminRouter = express.Router();
+// Router for /api/ 
+var apiRouter = express.Router();
 
-// Middleware to log something before page loads
-adminRouter.use(function(req, res, next){
-		console.log(req.method, req.url);
+// Middleware for /api/
+apiRouter.use(function(req, res, next){
+		console.log('Somebody just came to our app!');
+		//Next will go to the next route so it doesn't stop here.
 		next();
 });
 
-// Root of admin page
-adminRouter.get('/', function(req, res){
-		res.send('I am the dashboard!');
+// /api/ root 
+apiRouter.get('/', function(req, res) {
+		res.json({ message: 'Welcome to the API!' });
 });
 
-// /admin/users/ page, will wait for parameter :name
-adminRouter.get('/users/:name', function(req, res){
-		res.send('hello ' + req.params.name+ '!');
-});
+// Register routes
+app.use('/api', apiRouter);
 
-// /admin/users/ page
-adminRouter.get('/users', function(req, res){
-		res.send('I show all the users!');
-});
+app.listen(port);
 
-// Function to determine what to do for the :name parameter
-adminRouter.param('name', function(req, res, next, name){
-		console.log('doing name validations on ' + name);
-		req.name = name;
-		next();
-});
+console.log('Magic happens on port ' + port);
 
-// /admin/posts/ page
-adminRouter.get('/posts', function(req, res){
-		res.send('I show all the posts!');
-});
-
-// app will use adminRouter for all pages with /admin/ 
-app.use('/admin', adminRouter);
-
-// Listen on this port
-app.listen(1337);
-console.log('1337 is the magic port!');
+mongoose.connect('mongodb://localhost:27017/newDb');
