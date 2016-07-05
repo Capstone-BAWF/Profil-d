74215506b233ee5d3e8af8f0d667d457474d0524
Brandon Troche <bttroche@gmail.com>
2016-07-04 23:01:39 -0400

    Added functionality for counting the number of times a word is used in a tweet for Latent Semantic Analysis. This will allow me to better label important words in tweets due to repetition that could better help analyze the topic of the tweet.

---
+++ Tweet_Analysis/Testing.py
@@ -0,0 +1,32 @@
+import sys
+
+def TweetAnalysis(SemanticDictionary, Size, TweetArray):
+	for i in range(Size):
+		if(SemanticDictionary.has_key(TweetArray[i])):
+			SemanticDictionary[TweetArray[i]] += 1
+		else: 
+			SemanticDictionary[TweetArray[i]] = 1
+	print SemanticDictionary
+
+
+Tweet = ""
+while Tweet == "" or len(Tweet) > 150:
+	Tweet = raw_input("You should totally enter a tweet brah: ")
+	print(Tweet)
+	if len(Tweet) > 150:
+		print("Sorry that's not a tweet!")
+
+words = {}
+
+twitters = Tweet.split()
+tweetSize = len(twitters)
+
+print ("The size of the array is: " + str(tweetSize))
+print(twitters)
+print (len(Tweet))
+
+TweetAnalysis(words, tweetSize, twitters)
+
+"""for index in twitters:
+	print(index)"""
+
