4fd3098b41ed9353fa9628ceb08aba065bf36f61
William Wu <williamwu0013@gmail.com>
2016-07-04 20:12:14 -0400

    Updated entire web front end design. About Us page, new home page, navbar. Folders restructured.

---
+++ public/js/app.js
@@ -0,0 +1,42 @@
+angular.module('routerApp', ['routerRoutes'])
+
+.controller('mainController', function() {
+	var vm = this;
+})
+
+.controller('homeController', function() {
+	var vm = this;
+
+	vm.titles = [
+		{ name: 'Select a mode', function: 'Next' },
+		{ name: 'Pick a candidate', function: 'View results' },
+		{ name: 'Results', function: 'Try again' }
+	];
+})
+
+.controller('aboutController', function() {
+	var vm = this;
+
+	vm.members = [
+		{ name: 'William Wu', role: 'Full Stack Developer', 
+			bio: 'Interested in mobile development and experienced in full stack\
+				  development.',
+			contact: 'will.go.code@gmail.com',
+			pic: '/assets/images/will.jpg' },
+		{ name: 'Ada Wong', role: 'Front End Developer', 
+			bio: 'Recently came back from Miami. Continues to watch videos\
+				  on AngularJS.',
+			contact: 'ada77.chen@gmail.com',
+			pic: '/assets/images/ada.jpg' },
+		{ name: 'Fabio Francois', role: 'Back End Developer', 
+			bio: 'A candidate at Google, Fabio Francois plays a lot of games\
+				  when he is rarely working.',
+			contact: 'fabio.francois@gmail.com',
+			pic: '/assets/images/david.jpg' },	
+		{ name: 'Brandon Troche', role: 'iOS Developer', 
+			bio: 'Main focus is game development. Has a game on Apple\'s App Store\
+				  called Tap N Slash',
+			contact: 'bttroche@gmail.com',
+			pic: '/assets/images/brandon.jpg' }
+	];
+});
--- server.js
+++ server.js
@@ -1,56 +1,16 @@
-// Load modules
-var express = require('express'), app = express();
-var mongoose = require('mongoose');
-var bodyParser= require('body-parser');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var port = process.env.PORT || 8080;
+// get the things we need
+var express = require('express');
+var app     = express();
 var path = require('path');
-var User = require('./app/models/user');
 
-// Body parser module will allow us to grab information from POST requests
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+// set the public folder to serve public assets
+app.use(express.static(__dirname + '/public'));
 
-// Configure app to handle CORS requests
-app.use(function(req, res, next) {
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader('Access-Control-Allow-Methods', 'Get, POST');
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, \
-			Authorization');
-		next();
+// set up our one route to the index.html file
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
-// Log requests to console
-app.use(morgan('dev'));
-
-// Routes for API
-
-// Root page
-app.get('/', function(req, res){
-		res.sendFile(path.join(__dirname +'/index.html'));
-});
-
-// Router for /api/ 
-var apiRouter = express.Router();
-
-// Middleware for /api/
-apiRouter.use(function(req, res, next){
-		console.log('Somebody just came to our app!');
-		//Next will go to the next route so it doesn't stop here.
-		next();
-});
-
-// /api/ root 
-apiRouter.get('/', function(req, res) {
-		res.json({ message: 'Welcome to the API!' });
-});
-
-// Register routes
-app.use('/api', apiRouter);
-
-app.listen(port);
-
-console.log('Magic happens on port ' + port);
-
-mongoose.connect('mongodb://localhost:27017/newDb');
+// start the server on port 8080 (http://localhost:8080)
+app.listen(8080);
+console.log('Magic happens on port 8080.');
