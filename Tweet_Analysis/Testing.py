2a67d004d4b01a99b12e6b11efec236649b1284a
Brandon Troche <bttroche@gmail.com>
2016-07-07 13:15:23 -0400

    Style update.

--- Tweet_Analysis/Testing.py
+++ Tweet_Analysis/Testing.py
@@ -1,5 +1,31 @@
 import sys
 
+def main():
+	Tweet = ""
+	while Tweet == "" or len(Tweet) > 150:
+		Tweet = raw_input("You should totally enter a tweet brah: ")
+	print(Tweet)
+	if len(Tweet) > 150:
+		print("Sorry that's not a tweet!")
+
+	words = {}
+
+	twitters = Tweet.split()
+	tweetSize = len(twitters)
+
+	print ("The size of the array is: " + str(tweetSize))
+	print(twitters)
+	print (len(Tweet))
+
+	print ("The number of times each word was used: ")
+	WordAnalysis(words, tweetSize, twitters)
+	print ("The word that appeared the most was: " + WordCount(words))
+
+	"""for index in twitters:
+	print(index)"""
+
+
+
 def WordAnalysis(SemanticDictionary, Size, TweetArray):
 	for i in range(Size):
 		if(SemanticDictionary.has_key(TweetArray[i])):
@@ -24,26 +50,5 @@ def WordCount(SemanticDictionary):
 	#absoluteDeterminers = ["any", "all", "every"]
 
 
-Tweet = ""
-while Tweet == "" or len(Tweet) > 150:
-	Tweet = raw_input("You should totally enter a tweet brah: ")
-	print(Tweet)
-	if len(Tweet) > 150:
-		print("Sorry that's not a tweet!")
-
-words = {}
-
-twitters = Tweet.split()
-tweetSize = len(twitters)
-
-print ("The size of the array is: " + str(tweetSize))
-print(twitters)
-print (len(Tweet))
-
-print ("The number of times each word was used: ")
-WordAnalysis(words, tweetSize, twitters)
-print ("The word that appeared the most was: " + WordCount(words))
-
-"""for index in twitters:
-	print(index)"""
+main()
 
