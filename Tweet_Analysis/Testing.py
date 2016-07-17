b3472610deaf1064de48a40386c54a35df64ca36
Brandon Troche <bttroche@gmail.com>
2016-07-17 16:12:07 -0400

    Goal list updated, code cleaned up, unnecessary comment removal, additional words added to filter list, prototype stemming function added.

--- Tweet_Analysis/Testing.py
+++ Tweet_Analysis/Testing.py
@@ -7,7 +7,6 @@ import csv
 
 """
 -	Compute tf-idf using tf and df as separate tweets instead of separate documents
--	Filter out all the non-important words
 -	Compare two cvs's to each other. Say Hillary to Bernie and try to see how similar they are
 -	Group together all the stemmed words
 """
@@ -109,7 +108,7 @@ def termDocWeight(termFrequencyInDoc, totalTermsInDoc, termFreqInCorpus, totalDo
 	return tf*idf
 
 def stopWordsFilter(termArray):
-	stopWordList = "a about above after again against all am an and any are aren't as at be because been before being below between both but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're you've your yours yourself yourselves"
+	stopWordList = "a about above after again against all am an and any are aren't as at be because been before being below between both but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're you've your yours yourself yourselves just can"
 	stopWords = []
 	stopWords = stopWordList.split()
 	toRemove = []
@@ -124,46 +123,8 @@ def stopWordsFilter(termArray):
 	for objects in toRemove:
 		termArray.remove(objects)
 
-"""  
-import edu.stanford.nlp.pipeline._
-import edu.stanford.nlp.ling.CoreAnnotations._
-
-def createNLPPipeline(): StanfordCoreNLP = {
-	val props = new Properties()
-	props.put("annotators", "tokenize, ssplit, pos, lemma") new StanfordCoreNLP(props)
-}
-
-def isOnlyLetters(str: String): Boolean = { 
-
-	str.forall(c => Character.isLetter(c))
-
-}
-
-def plainTextToLemmas(text: String, stopWords: Set[String], pipeline: StanfordCoreNLP): Seq[String] = {
-	val doc = new Annotation(text) pipeline.annotate(doc)
-	val lemmas = new ArrayBuffer[String]()
-	val sentences = doc.get(classOf[SentencesAnnotation]) 
-	for (sentence <- sentences; token <- sentence.get(classOf[TokensAnnotation])): 
-		val lemma = token.get(classOf[LemmaAnnotation])
-		if (lemma.length > 2 && !stopWords.contains(lemma) && isOnlyLetters(lemma)):
-        	lemmas += lemma.toLowerCase
- 	return lemmas
-}
-
-val stopWords = sc.broadcast(
-scala.io.Source.fromFile("stopwords.txt).getLines().toSet).value
-val lemmatized: RDD[Seq[String]] = plainText.mapPartitions(it => { val pipeline = createNLPPipeline()
-it.map { case(title, contents) =>
-        plainTextToLemmas(contents, stopWords, pipeline)
-      }
-})
-"""
-
-#def DeterminerCount(TweetArray):
-	#generalDeterminers = ["the", "a", "an", "another", "no" , "'s", "my", "our", "their", "her"
-	#				, "his", "its", "each", "this", "that"]
-	#neutralDeterminers = ["certain", "there exist", "some of", "some"]
-	#absoluteDeterminers = ["any", "all", "every"]
+def stemming(termArray):
+	return 0
 
 
 main()
