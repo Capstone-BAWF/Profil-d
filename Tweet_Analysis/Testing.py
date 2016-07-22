604e76552e0764edc8d58f2abe443507241bef4b
Brandon Troche <bttroche@gmail.com>
2016-07-22 14:21:24 -0400

    helo dog

--- Tweet_Analysis/Testing.py
+++ Tweet_Analysis/Testing.py
@@ -4,7 +4,7 @@ import csv
 from math import log
 #import time
 #import tweepy
-#from pymongo import MongoClient 
+from pymongo import MongoClient 
 
 """
 -	Compare two cvs's to each other. Say Hillary to Bernie and try to see how similar they are
@@ -13,7 +13,7 @@ from math import log
 
 def main():
 
-	csvfile = open('hillary.csv', 'r')
+	csvfile = open('donny.csv', 'r')
 
 	reader = csv.DictReader(csvfile, fieldnames = ("name", "time", "tweets"))
 
@@ -77,7 +77,7 @@ def main():
 
 	mostUsed(100, wordsArray, wordsDictionary)
 
-	#print checkTerm("bernie", wordsDictionary)
+	#print checkTerm("wall", wordsDictionary)
 
 	
 
@@ -125,7 +125,19 @@ def termDocWeight(termFrequencyInDoc, totalTermsInDoc, termFreqInCorpus, totalDo
 	return tf*idf
 
 def stopWordsFilter(termArray):
-	stopWordList = "a about above after again against all am an and any are aren't as at be because been before being below between both but by can't cannot could couldn't did didn't do does doesn't doing don't down during each few for from further had hadn't has hasn't have haven't having he he'd he'll he's her here here's hers herself him himself his how how's i i'd i'll i'm i've if in into is isn't it it's its itself let's me more most mustn't my myself no nor not of off on once only or other ought our ours ourselves out over own same shan't she she'd she'll she's should shouldn't so some such than that that's the their theirs them themselves then there there's these they they'd they'll they're they've this those through to too under until up very was wasn't we we'd we'll we're we've were weren't what what's when when's where where's which while who who's whom why why's with won't would wouldn't you you'd you'll you're you've your yours yourself yourselves just can"
+	stopWordList = "a about above after again against all am an and any are \
+	aren't as at be because been before being below between both but by can't \
+	cannot could couldn't did didn't do does doesn't doing don't down during each \
+	few for from further had hadn't has hasn't have haven't having he he'd he'll he's\
+	 her here here's hers herself him himself his how how's i i'd i'll i'm i've if in \
+	 into is isn't it it's its itself let's me more most mustn't my myself no nor not of \
+	 off on once only or other ought our ours ourselves out over own same shan't she she'd \
+	 she'll she's should shouldn't so some such than that that's the their theirs them \
+	 themselves then there there's these they they'd they'll they're they've this those \
+	 through to too under until up very was wasn't we we'd we'll we're we've were weren't \
+	 what what's when when's where where's which while who who's whom why why's with won't \
+	 would wouldn't you you'd you'll you're you've your yours yourself yourselves just can"
+
 	stopWords = []
 	stopWords = stopWordList.split()
 	toRemove = []
